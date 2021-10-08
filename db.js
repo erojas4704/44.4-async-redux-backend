b44f39e95612cbfe82d81804a9b33536fc8f7596
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-10-08 03:15:01 -0400

    backend

---
+++ db.js
@@ -0,0 +1,11 @@
+/** Database connection for Microblog. */
+require('dotenv').config();
+
+const { Client } = require("pg");
+
+const client = new Client(process.env.DATABASE_URL || "postgresql:///microblog");
+
+client.connect();
+
+
+module.exports = client;
